dd131509f86a711c113d7fd4a6adff71b8fa9bd5
Igor Oliveira <igorfoliveira@gmail.com>
2020-06-14 01:24:03 -0300

    first commit

---
+++ src/main/java/com/ioliveira/SchedulingTasksApplication.java
@@ -0,0 +1,13 @@
+package com.ioliveira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingTasksApplication.class, args);
+	}
+
+}
